0ac3ff55c1e59231e56d7dfcea43292aaf388df1
ViktorVoloshko <viktor.voloshko@outlook.com>
2023-02-18 20:29:42 +0200

    Changes to repo tree

---
+++ backend/install-systemd-service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -f ~/.local/share/systemd/user/demo3.service ]; then
+    echo "systemd unit already installed!"
+else
+    mkdir -p ~/.local/share/systemd/user/
+    cp ./demo3.service ~/.local/share/systemd/user/demo3.service
+    systemctl enable petclinic --now
+    echo "systemd unit installed!"
+fi
