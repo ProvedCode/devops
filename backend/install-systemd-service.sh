532accdc95ad581880fe64db790924cba64ab3d5
ViktorVoloshko <viktor.voloshko@outlook.com>
2023-02-18 20:51:10 +0200

    Few more fixes to paths

--- backend/install-systemd-service.sh
+++ backend/install-systemd-service.sh
@@ -3,7 +3,7 @@
 if [ -f /usr/lib/systemd/user/demo3.service ]; then
     echo "systemd unit already installed!"
 else
-    sudo cp ./demo3.service /usr/lib/systemd/user/demo3.service
+    sudo cp ./backend/demo3.service /usr/lib/systemd/user/demo3.service
     sudo systemctl daemon-reload
     sudo systemctl enable petclinic --now
     echo "systemd unit installed!"
