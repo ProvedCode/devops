c61e81e62c1079cd541b6d5bbbb2011fda0d1059
ViktorVoloshko <viktor.voloshko@outlook.com>
2023-03-18 03:06:41 +0200

    Changed nginx source, removed ufw, use iptables instead

--- frontend/create_webserver.sh
+++ frontend/create_webserver.sh
@@ -5,13 +5,21 @@ SITES=(site1.com    site2.com   site3.com   site4.com   site5.com)
 PORTS=(8081         8082        8083        8084        8085)
 
 apt update
-apt install nginx -y
+apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring -y
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/debian `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list
+apt update
+apt install nginx
 systemctl status nginx
-apt install ufw -y
-echo "y" | sudo ufw enable
-ufw allow OpenSSH
-ufw allow 'Nginx HTTP'
-ufw status
+
+apt install iptables-persistent
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -I INPUT -p tcp --dport 22 -j ACCEPT
 
 echo "" > /etc/nginx/sites-available/sites.conf
 
@@ -19,7 +27,7 @@ for (( i=0; i<${#SITES[@]}; i++ ))
 do
 mkdir -p /var/www/${SITES[$i]}/html
 echo "Site $(( $i + 1 ))" > /var/www/${SITES[$i]}/html/index.html
-ufw allow ${PORTS[$i]}
+iptables -I INPUT -p tcp --dport ${PORTS[$i]} -j ACCEPT
 
 cat >> /etc/nginx/sites-available/sites.conf << EOF
 server {
@@ -41,7 +49,8 @@ if [ ! -f /etc/nginx/sites-enabled/sites.conf ]; then
     ln -s /etc/nginx/sites-available/sites.conf /etc/nginx/sites-enabled/sites.conf
 fi
 
+iptables-save > /etc/iptables/rules.v4
+
 nginx -t
 service nginx reload
 systemctl restart nginx
-
