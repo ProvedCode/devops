8fb420f2a0cc66f6f9240058ad605ba4b414055b
ViktorVoloshko <viktor.voloshko@outlook.com>
2023-03-23 03:08:48 +0200

    Fixed relative paths v2

--- backend/install-systemd-service.sh
+++ backend/install-systemd-service.sh
@@ -3,7 +3,7 @@
 if [ -f /etc/systemd/system/ProvedCode.service ]; then
     echo "systemd unit already installed!"
 else
-    sudo cp ./ProvedCode.service /etc/systemd/system/ProvedCode.service
+    sudo cp ./backend/ProvedCode.service /etc/systemd/system/ProvedCode.service
     sudo systemctl daemon-reload
     echo "systemd unit installed!"
 fi
